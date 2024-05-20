d199eacb79359bb0cf767fdd0444cd723078c298
Chen Shutong <shutong.23@intl.zju.edu.cn>
2024-05-21 04:26:44 +0800

    Practical8

--- Practical8/count_repeat_duplicate_genes.py
+++ Practical8/count_repeat_duplicate_genes.py
@@ -1,35 +1,43 @@
 import re
+import os
 
-# Ask user to input repetitive sequence
+#Ask user to input repetitive sequence
 repetitive_sequence = input("Please enter the repetitive sequence (GTGTGT or GTCTGT): ").strip().upper()
 
-# Create output filename based on repetitive sequence
+#Create output filename based on repetitive sequence
 output_filename = f"{repetitive_sequence}_duplicate_genes.fa"
 
-# Open the input file for reading and the output file for writing
-input_file = 'D:\桌面\IBI\IBI1_2023-24\Practical8\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
-with open(input_file, 'r') as input_f, open(output_filename, 'w') as output_f:
-    gene_name = ''
-    sequence = ''
+#Open the input file for reading
+os.chdir("D:\桌面\IBI\IBI1_2023-24\Practical8")
+input_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
 
-    for line in input_f:
-        line = line.strip()
+#Open the output file for writing
+output_file = open(output_filename, 'w')
 
-        if line.startswith('>'):
-            match = re.search(r'gene:(\S+)', line)
-            if match and repetitive_sequence in sequence:
-                # Write the previous gene's sequence to the output file
-                output_f.write(f'>{gene_name}\n{sequence}\n')
+#Define variables
+gene_name = ''
+sequence = ''
 
-            # Update the gene name and reset the sequence
-            gene_name = match.group(1)
-            sequence = ''
-        else:
-            # Append the sequence to the current gene
-            sequence += line
+#through each line in the input file
+for line in input_file:
+    line = line.strip()
+    if line.startswith(">"):
+        #If the sequence contains the repetitive element, write gene info to output file
+        if repetitive_sequence in sequence:
+            output_file.write(f'>{gene_name} {sequence.count(repetitive_sequence)}\n{sequence}\n\n')  #Add an extra newline between gene name and sequence
 
-    # Write the last gene's sequence to the output file
-    if gene_name and repetitive_sequence in sequence:
-        output_f.write(f'>{gene_name}\n{sequence}\n')
+        #Update gene name and reset sequence
+        gene_name = re.findall(r'gene:(.+)\sgene_biotype', line)[0]
+        sequence = ''
+    else:
+        sequence += line
+
+#Write last gene info to output file
+if repetitive_sequence in sequence:
+    output_file.write(f'>{gene_name} {sequence.count(repetitive_sequence)}\n{sequence}\n\n')  #Add an extra newline between gene name and sequence
+
+#Close input and output files
+input_file.close()
+output_file.close()
 
 print(f"Duplicate genes containing {repetitive_sequence} extracted and saved to '{output_filename}'")
--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,20 +1,19 @@
+#import library
 import re
+
+#set the input and output file
 input_file = 'D:\桌面\IBI\IBI1_2023-24\Practical8\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
 output_file = 'D:\桌面\IBI\IBI1_2023-24\Practical8\duplicate_genes.fa'
+
+#open the file
 with open(input_file, 'r') as input_f, open(output_file, 'w') as output_f:
-    gene_name = ''
-    sequence = ''
-    for line in input_f:
-        line = line.rstrip()
-        if line.startswith('>'):  
-            match = re.search(r'gene:(\S+)', line)
-            if match and 'duplication' in line:
-                if gene_name:
-                    output_f.write(f'>{gene_name}\n{sequence}\n')
-                gene_name = match.group(1)
-                sequence = ''
+     # find different sequence 
+     for line in input_f:
+        if line.startswith(">"):
+            if re.search('duplication', line):
+              name = re.findall(r'gene:(.+)\sgene_biotype', line) # see if it matches the regular expression
+              name = str(name) + '\n'
+              output_f.write(name) # write
         else:
-            sequence += line
-    if gene_name:
-        output_f.write(f'>{gene_name}\n{sequence}\n')
+            continue
 print("Duplicate genes extracted and saved to 'duplicate_genes.fa'")
